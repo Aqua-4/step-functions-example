c294703518f2baabf7b619bc1fe0d8e3d0d73c69
Parashar Sangle <parasharsangle@gmail.com>
2021-06-25 12:30:28 +0530

    FIX: errors and add x-ray tracing

--- handler.js
+++ handler.js
@@ -1,10 +1,13 @@
-'use strict';
+import * as AWSXRay from 'aws-xray-sdk-core';
+import * as AWSSdk from 'aws-sdk';
 
-module.exports.hello = (event, context, callback) => {
+AWSXRay.captureAWS(AWSSdk);
+
+export const hello = (event, context, callback) => {
   callback(null, null);
 };
 
-module.exports.ciao = (event, context, callback) => {
+export const ciao = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
